8a7c1f66540ec848e40c83e99962dfdecab1a09c
李晨玮 <lcw@meitu.com>
2018-11-23 18:16:53 +0800

    添加贴纸边框

--- app/src/main/java/com/lcw/view/StickerView.java
+++ app/src/main/java/com/lcw/view/StickerView.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -22,6 +25,14 @@ public class StickerView extends View implements View.OnTouchListener {
 
     private Bitmap mImageBitmap;
     private Matrix mImageMatrix;
+    private RectF mImageRectF;
+    private float[] mImagePoints;
+
+    private Paint mImageRectPaint;
+
+
+    private float mLastX;
+    private float mLastY;
 
     public StickerView(Context context) {
         super(context);
@@ -39,9 +50,24 @@ public class StickerView extends View implements View.OnTouchListener {
     }
 
     private void initView(Context context) {
+        //设置触摸监听
         setOnTouchListener(this);
+        //初始化图片，矩阵等信息
         mImageBitmap = BitmapFactory.decodeResource(context.getResources(), R.mipmap.icon);
         mImageMatrix = new Matrix();
+        mImageRectF = new RectF(0, 0, mImageBitmap.getWidth(), mImageBitmap.getHeight());
+        mImagePoints = new float[]{
+                0, 0,//左上
+                mImageBitmap.getWidth(), 0,//右上
+                mImageBitmap.getWidth(), mImageBitmap.getHeight(),//右下
+                0, mImageBitmap.getHeight(),//左下
+                mImageBitmap.getWidth() / 2, mImageBitmap.getHeight() / 2//中心点
+        };
+
+        //绘制图片外部矩形框
+        mImageRectPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mImageRectPaint.setColor(Color.BLACK);
+
     }
 
     @Override
@@ -57,10 +83,12 @@ public class StickerView extends View implements View.OnTouchListener {
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawBitmap(mImageBitmap, mImageMatrix, null);
+        float[] floats = getCurrentImagePoints();
+        canvas.drawLine(floats[0], floats[1], floats[2], floats[3], mImageRectPaint);
+        canvas.drawLine(floats[2], floats[3], floats[4], floats[5], mImageRectPaint);
+        canvas.drawLine(floats[4], floats[5], floats[6], floats[7], mImageRectPaint);
+        canvas.drawLine(floats[0], floats[1], floats[6], floats[7], mImageRectPaint);
     }
-
-    private float mLastX;
-    private float mLastY;
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
@@ -71,11 +99,13 @@ public class StickerView extends View implements View.OnTouchListener {
                 mLastY = event.getY();
                 break;
             case MotionEvent.ACTION_MOVE:
-                float currentX = event.getX();
-                float currentY = event.getY();
-                mImageMatrix.postTranslate(currentX - mLastX, currentY - mLastY);
-                mLastX = currentX;
-                mLastY = currentY;
+                if (isOnStickerView(mLastX, mLastY)) {//检测触摸点是否在贴纸上
+                    float currentX = event.getX();
+                    float currentY = event.getY();
+                    mImageMatrix.postTranslate(currentX - mLastX, currentY - mLastY);
+                    mLastX = currentX;
+                    mLastY = currentY;
+                }
                 break;
             case MotionEvent.ACTION_UP:
                 break;
@@ -84,4 +114,34 @@ public class StickerView extends View implements View.OnTouchListener {
         invalidate();
         return true;
     }
+
+
+    /**
+     * 检测当前触摸是否在贴纸上
+     *
+     * @param touchX
+     * @param touchY
+     * @return
+     */
+    private boolean isOnStickerView(float touchX, float touchY) {
+        float[] dstPoints = new float[2];
+        float[] srcPoints = new float[]{touchX, touchY};
+        Matrix matrix = new Matrix();
+        mImageMatrix.invert(matrix);
+        matrix.mapPoints(dstPoints, srcPoints);
+        return mImageRectF.contains(dstPoints[0], dstPoints[1]);
+    }
+
+    /**
+     * 获取当前贴纸所对应的关键点
+     *
+     * @return
+     */
+    private float[] getCurrentImagePoints() {
+        float[] dstPoints = new float[10];
+        float[] srcPoints = mImagePoints;
+        mImageMatrix.mapPoints(dstPoints, srcPoints);
+        return dstPoints;
+    }
+
 }
